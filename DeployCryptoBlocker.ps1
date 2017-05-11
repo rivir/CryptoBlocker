a177bc3907c577a58c7919d11e106a4387a4cd58
Tom <rivir@users.noreply.github.com>
2017-05-10 17:34:09 -0700

    Update DeployCryptoBlocker.ps1
    modify cryptoblocker script to push via array

--- DeployCryptoBlocker.ps1
+++ DeployCryptoBlocker.ps1
@@ -1,210 +1,173 @@
-# DeployCryptoBlocker.ps1
-#
+##CRYPTO BLOCKER TO ALL SERVERS in $servers array
+
+#SETUP ON SERVERS
+#PSv3 and REMOTE PS
+# $psversiontable
+#get-service winrm
+#Enable-PSRemoting –force
+#firewall rule remote FSRM
+
+$servers = @{SERVERNAME='DRIVELETTER'; SERVER2='D'}
+
+
+$fileGroupName = "1-PreventCrypto"
+$fileTemplateName = "1-PreventCrypto"
+$fileScreenName = "1-PreventCrypto"
+$eventConfFilename = "\\servername\cryptoblocker-eventnotify.txt"
+$mailConfFilename = "\\servername\cryptoblocker-mailnotify.txt"
+
+
+$webClient = New-Object System.Net.WebClient
+$jsonStr = $webClient.DownloadString("https://fsrm.experiant.ca/api/v1/get")
+$monitoredExtensions = @(ConvertFrom-Json20($jsonStr) | % { $_.filters })
+       
+        ######**********remove unwanted filters v5
+$Exclusions = @('*.stn','*.one')
+$monitoredExtensions = $monitoredExtensions | Where-Object { $Exclusions -notcontains $_ }
+        ######**********remove unwanted filters v5
+
+function Deploy-CryptoBlock
+{
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Position=0,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [Alias('hostname')]
+        [Alias('cn')]
+        [hashtable]$ComputerName = $env:COMPUTERNAME,
+        
+        [Parameter(Position=1,
+                   Mandatory=$false)]
+        [Alias('runas')]
+        [System.Management.Automation.Credential()]$Credential =
+        [System.Management.Automation.PSCredential]::Empty
+        
+    )
+    
+    BEGIN
+    {
+       
+
+    }
+    
+    PROCESS
+    {
+        foreach ($computer in $ComputerName.Keys|Sort)
+        {
+            try
+            {
+
+                    # Split the $monitoredExtensions array into fileGroups of less than 4kb to allow processing by filescrn.exe
+                    $fileGroups = New-CBArraySplit $monitoredExtensions
+                    ForEach ($group in $fileGroups) {
+                        $group | Add-Member -MemberType NoteProperty -Name fileGroupName -Value "$FileGroupName$($group.index)"
+                    }
+
+                    Write-Host "***" -foregroundcolor "magenta"
+                     Write-Host $computer -foregroundcolor "magenta"
+                      Write-Host "***" -foregroundcolor "magenta"
+                    # Perform these steps for each of the 4KB limit split fileGroups
+                    ForEach ($group in $fileGroups) {
+                        Write-Host "Adding/replacing File Group [$($group.fileGroupName)] with monitored file.." ##Too much text## [$($group.array -Join ",")].."
+                         &filescrn.exe filegroup Delete "/Filegroup:$($group.fileGroupName)" /Quiet /remote:$computer
+                         &filescrn.exe Filegroup Add "/Filegroup:$($group.fileGroupName)" "/Members:$($group.array -Join '|')" /remote:$computer
+                        
+                    }
+                    ##FALSE POSITIVE, EXEMPTIONS##
+                    &filescrn.exe Filegroup Modify "/Filegroup:$($group.fileGroupName)" "/NonMembers:*CrystalReports*" /remote:$computer
+                    
+                    Write-Host "Adding/replacing File Screen Template [$fileTemplateName] with Event Notification [$eventConfFilename] and Command Notification [$cmdConfFilename].."
+                    &filescrn.exe Template Delete /Template:$fileTemplateName /Quiet  /remote:$computer
+                    # Build the argument list with all required fileGroups
+                    $screenArgs = 'Template','Add',"/Template:$fileTemplateName"
+                    ForEach ($group in $fileGroups) {
+                        $screenArgs += "/Add-Filegroup:$($group.fileGroupName)"
+                    }
+
+                    &filescrn.exe $screenArgs /remote:$computer
+                    Write-Host "Adding/replacing File Screens.."
+                    $drivesContainingShares = $ComputerName.$computer+":\"
+                    $drivesContainingShares | % {
+                        Write-Host "`tAdding/replacing File Screen for [$_] with Source Template [$fileTemplateName].."
+                        &filescrn.exe Screen Delete "/Path:$_" /Quiet /remote:$computer         
+                        &filescrn.exe Screen Add "/Path:$_" "/SourceTemplate:$fileTemplateName" "/type:active" "/add-notification:m,$mailConfFilename" "/add-notification:e,$eventConfFilename" /remote:$computer
+              }
+            }
+            catch 
+            {
+                # Check for common DCOM errors and display "friendly" output
+                switch ($_)
+                {
+                    { $_.Exception.ErrorCode -eq 0x800706ba } `
+                        { $err = 'Unavailable (Host Offline or Firewall)'; 
+                            break; }
+                    { $_.CategoryInfo.Reason -eq 'UnauthorizedAccessException' } `
+                        { $err = 'Access denied (Check User Permissions)'; 
+                            break; }
+                    default { $err = $_.Exception.Message }
+                }
+                Write-Warning "$computer - $err" -foregroundcolor "red"
+            } 
+        }
+    }
+    
+    END {}
+}
+
+
+Deploy-CryptoBlock -cn $servers -Verbose
+
 ################################ Functions ################################
 
-Function ConvertFrom-Json20
+function ConvertFrom-Json20([Object] $obj)
 {
-    # Deserializes JSON input into PowerShell object output
-    Param (
-        [Object] $obj
-    )
     Add-Type -AssemblyName System.Web.Extensions
     $serializer = New-Object System.Web.Script.Serialization.JavaScriptSerializer
     return ,$serializer.DeserializeObject($obj)
 }
 
-Function New-CBArraySplit
-{
-    <# 
-        Takes an array of file extensions and checks if they would make a string >4Kb, 
-        if so, turns it into several arrays
-    #>
+Function New-CBArraySplit {
+
     param(
-        $Extensions
+        $extArr,
+        $depth = 1
     )
 
-    $Extensions = $Extensions | Sort-Object -Unique
+    $extArr = $extArr | Sort-Object -Unique
+    # Concatenate the input array
+    $conStr = $extArr -join ','
+    $outArr = @()
 
-    $workingArray = @()
-    $WorkingArrayIndex = 1
-    $LengthOfStringsInWorkingArray = 0
-
-    # TODO - is the FSRM limit for bytes or characters?
-    #        maybe [System.Text.Encoding]::UTF8.GetBytes($_).Count instead?
-    #        -> in case extensions have Unicode characters in them
-    #        and the character Length is <4Kb but the byte count is >4Kb
-
-    # Take the items from the input array and build up a 
-    # temporary workingarray, tracking the length of the items in it and future commas
-    $Extensions | ForEach-Object {
-
-        if (($LengthOfStringsInWorkingArray + 1 + $_.Length) -gt 4096) 
-        {   
-            # Adding this item to the working array (with +1 for a comma)
-            # pushes the contents past the 4Kb limit
-            # so output the workingArray
-            [PSCustomObject]@{
-                index = $WorkingArrayIndex
-                FileGroupName = "$Script:FileGroupName$WorkingArrayIndex"
-                array = $workingArray
-            }
-            
-            # and reset the workingArray and counters
-            $workingArray = @($_) # new workingArray with current Extension in it
-            $LengthOfStringsInWorkingArray = $_.Length
-            $WorkingArrayIndex++
-
+    # If the input string breaks the 4Kb limit
+    If ($conStr.Length -gt 4096) {
+        # Pull the first 4096 characters and split on comma
+        $conArr = $conStr.SubString(0,4096).Split(',')
+        # Find index of the last guaranteed complete item of the split array in the input array
+        $endIndex = [array]::IndexOf($extArr,$conArr[-2])
+        # Build shorter array up to that indexNumber and add to output array
+        $shortArr = $extArr[0..$endIndex]
+        $outArr += [psobject] @{
+            index = $depth
+            array = $shortArr
         }
-        else #adding this item to the workingArray is fine
-        {
-            $workingArray += $_
-            $LengthOfStringsInWorkingArray += (1 + $_.Length)  #1 for imaginary joining comma
-        }
+
+        # Then call this function again to split further
+        $newArr = $extArr[($endindex + 1)..($extArr.Count -1)]
+        $outArr += New-CBArraySplit $newArr -depth ($depth + 1)
+        
+        return $outArr
     }
-
-    # The last / only workingArray won't have anything to push it past 4Kb
-    # and trigger outputting it, so output that one as well
-    [PSCustomObject]@{
-        index = ($WorkingArrayIndex)
-        FileGroupName = "$Script:FileGroupName$WorkingArrayIndex"
-        array = $workingArray
+    # If the concat string is less than 4096 characters already, just return the input array
+    Else {
+        return [psobject] @{
+            index = $depth
+            array = $extArr
+        }  
     }
 }
 
 ################################ Functions ################################
 
-# Get all drives with shared folders, these drives will get FRSRM protection
-$DrivesContainingShares = @(Get-WmiObject Win32_Share |            # all shares on this computer, filter:
-                            Where-Object { $_.Type -eq 0 } |       # 0 = disk drives (not printers, IPC$, C$ Admin shares)
-                            Select-Object -ExpandProperty Path |    # Shared folder path, e.g. "D:\UserFolders\"
-                            ForEach-Object { 
-                                ([System.IO.DirectoryInfo]$_).Root.Name  # Extract the driveletter, as a string
-                            } | Sort-Object -Unique)               # remove duplicates
 
-
-if ($drivesContainingShares.Count -eq 0)
-{
-    Write-Host "No drives containing shares were found. Exiting.."
-    exit
-}
-
-Write-Host "The following shares needing to be protected: $($drivesContainingShares -Join ",")"
-
-
-#### Identify Windows Server version, and install FSRM role
-$majorVer = [System.Environment]::OSVersion.Version.Major
-$minorVer = [System.Environment]::OSVersion.Version.Minor
-
-Write-Host "Checking File Server Resource Manager.."
-
-Import-Module ServerManager
-
-if ($majorVer -ge 6)
-{
-    $checkFSRM = Get-WindowsFeature -Name FS-Resource-Manager
-
-    if ($minorVer -ge 2 -and $checkFSRM.Installed -ne "True")
-    {
-        # Server 2012
-        Write-Host "FSRM not found.. Installing (2012).."
-        Install-WindowsFeature -Name FS-Resource-Manager -IncludeManagementTools
-    }
-    elseif ($minorVer -ge 1 -and $checkFSRM.Installed -ne "True")
-    {
-        # Server 2008 R2
-        Write-Host "FSRM not found.. Installing (2008 R2).."
-        Add-WindowsFeature FS-FileServer, FS-Resource-Manager
-    }
-    elseif ($checkFSRM.Installed -ne "True")
-    {
-        # Server 2008
-        Write-Host "FSRM not found.. Installing (2008).."
-        &servermanagercmd -Install FS-FileServer FS-Resource-Manager
-    }
-}
-else
-{
-    # Assume Server 2003
-    Write-Host "Unsupported version of Windows detected! Quitting.."
-    return
-}
-
-
-$fileGroupName = "CryptoBlockerGroup"
-$fileTemplateName = "CryptoBlockerTemplate"
-$fileScreenName = "CryptoBlockerScreen"
-
-# Download list of CryptoLocker file extensions
-$webClient = New-Object System.Net.WebClient
-$jsonStr = $webClient.DownloadString("https://fsrm.experiant.ca/api/v1/get")
-$monitoredExtensions = @(ConvertFrom-Json20 $jsonStr | ForEach-Object { $_.filters })
-
-If (Test-Path .\SkipList.txt)
-{
-    $Exclusions = Get-Content .\SkipList.txt | ForEach-Object { $_.Trim() }
-    $monitoredExtensions = $monitoredExtensions | Where-Object { $Exclusions -notcontains $_ }
-
-}
-Else 
-{
-    $emptyFile = @'
-#
-# Add one filescreen per line that you want to ignore
-#
-# For example, if *.doc files are being blocked by the list but you want 
-# to allow them, simply add a new line in this file that exactly matches 
-# the filescreen:
-#
-# *.doc
-#
-# The script will check this file every time it runs and remove these 
-# entries before applying the list to your FSRM implementation.
-#
-'@
-    Set-Content -Path .\SkipList.txt -Value $emptyFile
-}
-
-
-# Split the $monitoredExtensions array into fileGroups of less than 4kb to allow processing by filescrn.exe
-$fileGroups = @(New-CBArraySplit $monitoredExtensions)
-
-# Perform these steps for each of the 4KB limit split fileGroups
-ForEach ($group in $fileGroups) {
-    Write-Host "Adding/replacing File Group [$($group.fileGroupName)] with monitored file [$($group.array -Join ",")].."
-    &filescrn.exe filegroup Delete "/Filegroup:$($group.fileGroupName)" /Quiet
-    &filescrn.exe Filegroup Add "/Filegroup:$($group.fileGroupName)" "/Members:$($group.array -Join '|')"
-}
-
-Write-Host "Adding/replacing File Screen Template [$fileTemplateName] with Event Notification [$eventConfFilename] and Command Notification [$cmdConfFilename].."
-&filescrn.exe Template Delete /Template:$fileTemplateName /Quiet
-# Build the argument list with all required fileGroups
-$screenArgs = 'Template', 'Add', "/Template:$fileTemplateName"
-ForEach ($group in $fileGroups) {
-    $screenArgs += "/Add-Filegroup:$($group.fileGroupName)"
-}
-
-&filescrn.exe $screenArgs
-
-$EmailNotification = $env:TEMP + "\tmpEmail001.tmp"
-$EventNotification = $env:TEMP + "\tmpEvent001.tmp"
-
-# Write the email options to the temporary file
-"Notification=m" >> $EmailNotification
-"To=[Admin Email]" >> $EmailNotification
-"Subject=Unauthorized file from the [Violated File Group] file group detected" >> $EmailNotification
-"Message=User [Source Io Owner] attempted to save [Source File Path] to [File Screen Path] on the [Server] server. This file is in the [Violated File Group] file group, which is not permitted on the server."  >> $EmailNotification
-
-# Write the event log options to the temporary file
-"Notification=e" >> $EventNotification
-"EventType=Warning" >> $EventNotification
-"Message=User [Source Io Owner] attempted to save [Source File Path] to [File Screen Path] on the [Server] server. This file is in the [Violated File Group] file group, which is not permitted on the server." >> $EventNotification
-
-Write-Host "Adding/replacing File Screens.."
-$drivesContainingShares | ForEach-Object {
-    Write-Host "`tAdding/replacing File Screen for [$_] with Source Template [$fileTemplateName].."
-    &filescrn.exe Screen Delete "/Path:$_" /Quiet
-    &filescrn.exe Screen Add "/Path:$_" "/SourceTemplate:$fileTemplateName" "/Add-Notification:m,$EmailNotification" "/Add-Notification:e,$EventNotification"
-}
-
-Remove-Item $EmailNotification -Force
-Remove-Item $EventNotification -Force
